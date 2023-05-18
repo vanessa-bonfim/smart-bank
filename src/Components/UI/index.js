7edc2404e0d5f71419ee02b69085fa8b73297015
Vanessa Bonfim <vanesssabonfim@gmail.com>
2023-05-18 14:31:59 +0100

    feat: Create UI component; imp: Import Google Font inside html;

---
+++ src/Components/UI/index.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Icon = styled.img`
+  height: 25px;
+  width: 25px;
+    
+`
---
+++ src/Components/UI/variaveis.js
@@ -0,0 +1,3 @@
+export const primaryColor = '#41d3be';
+export const backgroundLight = '#f1f1f1';
+export const backgroundTextLight = 'grey';
